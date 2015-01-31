38ec14defbf045b360e0d672c75f8d7f3f9a7d91
mobjohnson <mobjohnson@gmail.com>
2015-01-31 14:16:29 -0500

    Add bonus to pyramid.rb

--- class02/pyramid/pyramid.rb
+++ class02/pyramid/pyramid.rb
@@ -2,14 +2,17 @@ puts "Please enter a character to make the pyramid"
 char = gets.chomp
 puts "How many rows in the pyramid?"
 rows = gets.chomp.to_i
-
-
+puts "Do you want an upside-down pyramid? (Y or N)"
+upside_down = gets.chomp 
 count = 0
-
-
 while rows > count
-  print "#{' ' * (rows - count)}"
-  puts "#{char}" * (count * 2 + 1)
+  if upside_down == 'n'
+    print "#{' ' * (rows - count)}"
+    puts "#{char}" * (count * 2 + 1)
+  else
+    print "#{' ' * count}"
+    puts "#{char}" * ((rows-count) * 2 -1)
+  end
   count += 1
 end
 
