cb4d3115e4e191055aef74e14a48d0fa23184aab
mobjohnson <mobjohnson@gmail.com>
2015-01-31 13:54:13 -0500

    Remove row decrement from pyramid.rb

--- class02/pyramid/pyramid.rb
+++ class02/pyramid/pyramid.rb
@@ -2,13 +2,14 @@ puts "Please enter a character to make the pyramid"
 char = gets.chomp
 puts "How many rows in the pyramid?"
 rows = gets.chomp.to_i
-line = 0
-output = char
-while rows > 0
-  print "#{' ' * rows}"
-  puts "#{char}" * (line * 2 + 1)
-  # output = output + char + output
-  line += 1
-  rows -= 1
+
+
+count = 0
+
+
+while rows > count
+  print "#{' ' * (rows - count)}"
+  puts "#{char}" * (count * 2 + 1)
+  count += 1
 end
 
