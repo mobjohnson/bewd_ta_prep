894284cab60e6bb4f783e6dc1f435034e1d9d1a0
mobjohnson <mobjohnson@gmail.com>
2015-01-31 20:15:03 -0500

    Add lunch_order solution

---
+++ class03/lunch_orders/solution/lunch_orders_fin.rb
@@ -0,0 +1,27 @@
+orders = {}
+
+puts "Add an item to the order? (y/n)"
+while gets.chomp != 'n'
+  puts "Name for order:"
+  name = gets.chomp
+
+  puts "#{name} wants to order:"
+  order = gets.chomp
+
+  orders[name] ||= []
+  orders[name].push(order)
+
+  puts "Add another item to the order? (y/n)"
+end
+
+puts "\nAll orders:"
+
+
+orders.each_pair do |key, values|
+  if values.length == 1
+    puts "#{key} wants a #{values[0]}"
+  elsif values.length > 1
+    last = values.pop
+    puts "#{key} wants a #{values.join(', ')} & #{last}"
+  end
+end
