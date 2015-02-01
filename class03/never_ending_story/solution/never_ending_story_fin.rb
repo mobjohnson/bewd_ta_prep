2e944ef6bc2a7f426c112db98d81dd910e753dbb
mobjohnson <mobjohnson@gmail.com>
2015-01-31 21:05:52 -0500

    Add never_ending_story solution and comments class03

--- class03/never_ending_story/solution/never_ending_story_fin.rb
+++ class03/never_ending_story/solution/never_ending_story_fin.rb
@@ -6,17 +6,10 @@ story = [
   'Fry encounters his grandmother, then inadvertently sires his father'
 ]
 
-i = 0
-puts story[i]
-puts 'Should we continue? (Y/N)'
-while gets.chomp.downcase != 'n'
-  i += 1
-  puts story[i]
-  puts 'Should we continue? (Y/N)'
-  i = -1 if i == (story.length - 1)
+index = 0
+
+while gets.chomp != "n"
+  puts story[index % story.length]
+  index =+ 1
+  puts "Should we continue? (y/n)"
 end
-
-
-### solution file does not work
-### it does not prompt prior to the gets.chomp
-### and has =+ instead of +=
