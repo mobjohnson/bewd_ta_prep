a52e9ebfbcbd1f30bcc522e07d4b94325e0fa64c
mobjohnson <mobjohnson@gmail.com>
2015-02-11 11:35:28 -0500

    Finish padawans

--- class05/jedi_academy/padawans.rb
+++ class05/jedi_academy/padawans.rb
@@ -10,7 +10,34 @@ ActiveRecord::Base.establish_connection("postgres://localhost/jedi_academy")
 
 class Padawan < ActiveRecord::Base
 
+def randomize_lightsaber
+  color = ['red', 'blue', 'orange', 'aqua']
+  self.update(lightsaber: color.sample)
+  self.save
+  p self
 end
 
 
+end
+
+p = Padawan.find(8)
+
+p.randomize_lightsaber
+
+
 binding.pry
+
+# Padawan.all
+
+# Padawan.where(lightsaber: 'purple').order(:name)
+
+# p = Padawan.where(lightsaber: 'purple')
+# p.each do |single|
+#   single.update(:lightsaber => 'pink')
+  
+# end
+
+p = Padawan.find(8)
+
+
+
