93006adf21f330d43b585e1fda637d8ca88e1b88
mobjohnson <mobjohnson@gmail.com>
2015-02-12 20:08:15 -0500

    Finish Bonus 1 mona homework class06

--- class06/moma/moma.rb
+++ class06/moma/moma.rb
@@ -6,10 +6,16 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection("postgres://localhost/moma_db")
 
 class Artist < ActiveRecord::Base
+  validates :name, presence: true, length: {minimum: 1}, uniqueness: true
+  validates :nationality, presence: true
+
   has_many :paintings, :dependent => :destroy
 end
 
 class Painting < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 1}
+  validates_associated :artist
+
   belongs_to :artist
 end
 
