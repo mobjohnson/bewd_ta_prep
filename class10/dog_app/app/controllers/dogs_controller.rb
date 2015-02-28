568290f511fa95cff4b9330156847a5df9881918
mobjohnson <mobjohnson@gmail.com>
2015-02-28 09:36:54 -0500

    Add dogs index controller and view

---
+++ class10/dog_app/app/controllers/dogs_controller.rb
@@ -0,0 +1,34 @@
+class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+    render :index
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+
+end
