b5b3beae1fa72aedcdf459c837095c1baba5ddbd
mobjohnson <mobjohnson@gmail.com>
2015-03-04 17:03:22 -0500

    reorganize folders

--- warmup01class12/WarmupINSTRUCT/warmup/starter/sort_solution.rb
+++ warmup01class12/WarmupINSTRUCT/warmup/starter/sort_solution.rb
@@ -1,15 +1,12 @@
-## Arrays, sorting and blocks
 
-require 'pry'
+# Arrays, sorting and blocks
 
 random_animals = ["porpoise", "camel", "lobster", "kangaroo", "wombat", "chameleon"]
-
-p random_animals
 
 # Print out a plural version of each animal on a new line(ie porpoises, camels, etc)
 
 random_animals.each do |animal|
-  puts animal + 's'
+  puts animal + "s"
 end
 
 # Return an array of the animals sorted alphabetically
@@ -18,25 +15,32 @@ random_animals.sort
 
 # Return an array of the animals sorted reverse alphabetically
 
-random_animals.sort.reverse
+random_animals.sort do |a, b|
+  b <=> a
+end
 
 # Return an array of the animals with each individual string reversed
-random_animals.map do |animal|  
+
+random_animals.map do |animal|
   animal.reverse
 end
 
 # Return an array of the animals sorted by word length (low to high)
+
 random_animals.sort_by do |animal|
   animal.length
 end
 
 # Return an array of the animals sorted alphabetically by the last character in the string
+
 random_animals.sort_by do |animal|
   animal[-1]
 end
 
 
-binding.pry
+
+
+
 
 
 
--- warmup01class12/try01/warmup/sort_methods.rb
+++ warmup01class12/try01/warmup/sort_methods.rb
@@ -4,9 +4,18 @@ sen = "We are never ever ever getting back together"
 
 puts sen
 
+def reverse_words(sentence)
+  sentence.split(' ').reverse.join(' ')
+end
 
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(' ')
+  rev_sentence_array = sentence_array.map do |word|
+    word.reverse
+  end
+  rev_sentence_array.join(' ')
+end
 
+puts reverse_words(sen)
 
-# puts reverse_words(sen)
-
-# puts reverse_each_word(sen)
+puts reverse_each_word(sen)
