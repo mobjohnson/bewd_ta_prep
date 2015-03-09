c3f1f132677883249766e49e1b0a7306989fc65b
mobjohnson <mobjohnson@gmail.com>
2015-03-08 21:02:04 -0400

    Add trailers migration

--- class12/movie_trailer/db/schema.rb
+++ class12/movie_trailer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308145255) do
+ActiveRecord::Schema.define(version: 20150308172932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20150308145255) do
     t.string  "title"
     t.integer "year"
     t.text    "poster_url"
+  end
+
+  create_table "trailers", force: true do |t|
+    t.string  "title"
+    t.integer "embed_url"
+    t.integer "movie_id"
   end
 
 end
